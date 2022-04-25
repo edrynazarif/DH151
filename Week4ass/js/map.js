3015a20d30eda29ea7dd2db2b03c3875097bcea4
edrynazarif <edrynazarif@gmail.com>
2022-04-24 22:47:08 -0700

    Week4assignment

--- Week4ass/js/map.js
+++ Week4ass/js/map.js
@@ -12,8 +12,7 @@ let markers = L.featureGroup();
 // path to csv data
 let path = "data/minwage.csv";
 
-// Introducing initialize
-// When user hits your website and it loads, anything that goes inside "document.ready" is executed. For now, we have instructed 2 functions
+// initialize
 
 // createMap is a function that is defined below 
 $( document ).ready(function() {
@@ -53,32 +52,15 @@ function readCSV(path){
 }
     // after data is read, it executes function using the data 
     // function mapCSV(data){
-	
-	// loop through each entry. because we're using papa parse data, we must name "data.data"
-	    // data.data.forEach(function(item,index){
-		// create marker
-		// let marker = L.marker([item.latitude,item.longitude])
 
-		// add marker to featuregroup
-		//markers.addLayer(marker)
-	// })
-
-	// add featuregroup to map
-	//markers.addTo(map)
-
-	// fit markers to map so that it zooms out to all the points in your data
-	//map.fitBounds(markers.getBounds())
-
-
-    // alternatively, we can use circle markers and rewrite function mapCSV (data) as 
-function mapCSV(data){
+    function mapCSV(data){
 	
         // circle options
         let circleOptions = {
             radius: 5,
             weight: 1,
-            color: 'black',
-            fillColor: 'red',
+            color: 'purple',
+            fillColor: 'lavender',
             fillOpacity: 1
         }
     
@@ -88,10 +70,10 @@ function mapCSV(data){
             let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
 
             // Adding on a hover event (instead of onclick)
-            //.on('mouseover',function(){
-                this.bindPopup(`${item.title}<br>${item.minwage}</br>`).openPopup()
+             .on('mouseover',function(){
+                this.bindPopup(`${item.title}<br>${item.minwage}`).openPopup()
             
-            //})
+            })
     
             // add marker to featuregroup		
             markers.addLayer(marker) 
@@ -101,5 +83,4 @@ function mapCSV(data){
         markers.addTo(map)
     
         // fit markers to map
-        map.fitBounds(markers.getBounds())
-     }
+        map.fitBounds(markers.getBounds())}
