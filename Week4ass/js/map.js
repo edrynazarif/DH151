43e7708afb3f9925ddc990dc986b57300d72b8e3
edrynazarif <edrynazarif@gmail.com>
2022-04-24 17:56:14 -0700

    Week4assignment

---
+++ Week4ass/js/map.js
@@ -0,0 +1,113 @@
+// Working with csv files 
+// Introducing Global variables
+// Global variables are variables that you can use repeatedly
+// zl is zoom level 
+let map;
+let lat = 0;
+let lon = 0;
+let zl = 3;
+// global variables
+let markers = L.featureGroup();
+
+// path to csv data
+let path = "data/minwage.csv";
+
+// Introducing initialize
+// When user hits your website and it loads, anything that goes inside "document.ready" is executed. For now, we have instructed 2 functions
+
+// createMap is a function that is defined below 
+$( document ).ready(function() {
+    createMap(lat,lon,zl);
+    // after creating Map, read CSV
+    readCSV(path);
+});
+
+// create the map function
+// lat, lon, zl were defined in global variables above 
+function createMap(lat,lon,zl){
+	map = L.map('map').setView([lat,lon], zl);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+// read csv data function 
+function readCSV(path){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+            // spit it out onto the console 
+			console.log(data);
+			
+			// map the data
+			mapCSV(data);
+
+		}
+	});
+}
+    // after data is read, it executes function using the data 
+    function mapCSV(data){
+	
+	// loop through each entry. because we're using papa parse data, we must name "data.data"
+	    // data.data.forEach(function(item,index){
+		// create marker
+		// let marker = L.marker([item.latitude,item.longitude])
+
+		// add marker to featuregroup
+		//markers.addLayer(marker)
+	// })
+
+	// add featuregroup to map
+	//markers.addTo(map)
+
+	// fit markers to map so that it zooms out to all the points in your data
+	//map.fitBounds(markers.getBounds())
+}
+
+    // alternatively, we can use circle markers and rewrite function mapCSV (data) as 
+function mapCSV(data){
+	
+        // circle options
+        let circleOptions = {
+            radius: 5,
+            weight: 1,
+            color: 'white',
+            fillColor: 'dodgerblue',
+            fillOpacity: 1
+        }
+    
+        // loop through each entry
+        data.data.forEach(function(item,index){
+            // create marker
+            let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
+
+            // Adding on a hover event (instead of onclick)
+            .on('mouseover',function(){
+                this.bindPopup(`${item.title}<br><img src="${item.thumbnail_url}">`).openPopup()
+            
+            })
+    
+            // add marker to featuregroup		
+            markers.addLayer(marker)
+
+            // add entry to sidebar
+            $('.sidebar').append(`<img src="${item.thumbnail_url}" onmouseover="panToImage(${index})">`)
+            })
+    
+        // add featuregroup to map
+        markers.addTo(map)
+    
+        // fit markers to map
+        map.fitBounds(markers.getBounds())
+        }
+
+        // Make it so that when we hover over the image, it will take us to the marker
+
+        function panToImage(index){
+            // zoom to level 17 first
+            map.setZoom(17);
+            // pan to the marker
+            map.panTo(markers.getLayers()[index]._latlng);
+        }
